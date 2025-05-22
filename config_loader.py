3a2681df855fba1ec81aa74f27f9b805c2edb0aa
GanizaniSitara <dcx45@hotmail.com>
2025-05-22 16:25:40 +0100

    audit updates

--- config_loader.py
+++ config_loader.py
@@ -35,19 +35,9 @@ def load_visualization_settings(config_path='settings.ini'):
     if os.path.exists(config_path):
         config.read(config_path)
     
-    # If base_url is not specified but api_base_url is, derive base_url from api_base_url
-    if not config['confluence'].get('base_url') and config['confluence'].get('api_base_url'):
-        api_base_url = config['confluence'].get('api_base_url')
-        # Remove trailing /rest/api if present
-        if '/rest/api' in api_base_url:
-            base_url = api_base_url.split('/rest/api')[0]
-        else:
-            base_url = api_base_url
-        config['confluence']['base_url'] = base_url
-    
     # Return as dictionary
     return {
-        'confluence_base_url': config['confluence'].get('base_url'),
+        'base_url': config['confluence'].get('base_url'),
         'default_clusters': int(config['visualization'].get('default_clusters')),
         'default_min_pages': int(config['visualization'].get('default_min_pages'))
     }
